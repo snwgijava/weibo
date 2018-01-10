e6c6cecc7d4f31bb9c74ca374d791318308bc96f
Your Name <809127232@qq.com>
2018-01-10 14:00:03 +0000

    将用户模型移动到Models文件夹中

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable  //Authenticatable 授权相关功能的引用
 {
+    //消息通知相关功能引用
     use Notifiable;
 
     /**
      * The attributes that are mass assignable.
-     *
+     *过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新
      * @var array
      */
     protected $fillable = [
@@ -20,7 +21,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     *对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏，则可使用 hidden 属性
      * @var array
      */
     protected $hidden = [
